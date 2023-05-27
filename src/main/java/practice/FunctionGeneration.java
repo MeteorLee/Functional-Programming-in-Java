aa182ba675195b8ff026499d1083a14858ff4df4
MeteorLee <meteorproject529@gmail.com>
2023-05-27 23:37:02 +0900

    refactor: 람다 표현식을 이용해서 Function함수 리팩토링
    람다식에 따라서 생략가능한 부분들이 있음

--- src/main/java/practice/FunctionGeneration.java
+++ src/main/java/practice/FunctionGeneration.java
@@ -4,12 +4,12 @@ import practice.util.Adder;
 
 import java.util.function.Function;
 
-public class Main {
+public class FunctionGeneration {
     public static void main(String[] args) {
 
         Function<Integer, Integer> myAdder = new Adder();
         int result = myAdder.apply(5);
         System.out.println("result = " + result);
-        
+
     }
 }
---
+++ src/main/java/practice/LambdaGenerate.java
@@ -0,0 +1,19 @@
+package practice;
+
+import java.util.function.Function;
+
+public class LambdaGenerate {
+    public static void main(String[] args) {
+
+        Function<Integer, Integer> lambdaAdder1 = (Integer x) -> {
+            return x + 10;
+        };
+        int result1 = lambdaAdder1.apply(5);
+        System.out.println("result = " + result1);
+
+        Function<Integer, Integer> lambdaAdder2 = x -> x + 10;
+        int result2 = lambdaAdder2.apply(5);
+        System.out.println("result1 = " + result2);
+
+    }
+}
